7938f1d8b94990512b8e5f99a3db159696fdde21
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-12 21:38:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ return Object.assign({}, object, {[key]:value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-object.key= value
+object[key]=value
  return object
 }
 
-function 
+function deleteFromObjectByKey(object, key){
+  
+}
